98d02fb18b1c86091dd9f3dbb9cdb19f41001aaf
Soetiee2207 <nguyenminh2207tlu@gmail.com>
2026-05-06 22:46:59 +0700

    fix add product_2

--- backend/models/Service.js
+++ backend/models/Service.js
@@ -14,7 +14,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: true,
     },
     price: {
-      type: DataTypes.DECIMAL(10, 2),
+      type: DataTypes.BIGINT,
       allowNull: false,
     },
     duration: {
